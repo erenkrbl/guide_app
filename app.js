c9866a17a4dba29b022c5dbbec436fbce2231eeb
Joseph Abraham <ekurubal24@gmail.com>
2021-04-08 22:00:54 +0300

    Js codes written

---
+++ app.js
@@ -0,0 +1,68 @@
+// Let's choose interface
+
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+
+const form = document.getElementById('form-guide');
+
+// event listeners define
+
+form.addEventListener('submit', Submit);
+
+function Submit(e) {
+    e.preventDefault();
+
+    const addPerson = {
+        firstname : firstname.value,
+        lastname : lastname.value,
+        email : email.value,
+    }
+    const result = dataCheck(addPerson);
+    if (result.statu) {
+
+    } else {
+        infoCreate(result.message, result.statu)
+        console.log(result.message);
+    }
+
+    console.log(addPerson);
+}
+
+function dataCheck(person) {
+    // use in  at objects
+    for (const value in person) {
+        if (person[value]) {
+            console.log(person[value])
+        } else {
+            const result = {
+                statu : false,
+                message : 'Do not empty'  
+            }
+            return result;
+        }
+    }
+
+    return {
+        statu : true,
+        message : ''
+    }
+
+}
+
+function infoCreate  (message, statu) {
+    const createdInfo = document.createElement('div');
+    createdInfo.textContent = message;
+    createdInfo.className = 'info';
+
+    // if (statu) {
+    //     createdInfo.classList.add('info--success');
+    // } else {
+    //     createdInfo.classList.add('info--error')
+    // }
+
+    createdInfo.classList.add( statu ? 'info--success' : 'info--error' );
+
+
+    document.querySelector('.container').insertBefore(createdInfo, form);
+}
